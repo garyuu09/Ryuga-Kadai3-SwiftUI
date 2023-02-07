4f96052447b356ae6cb06083c243d7f2e359d14f
Ryuga <ribereo.minami@gmail.com>
2023-02-07 22:34:45 +0900

    トグルスイッチを実装

--- Kadai3/ContentView.swift
+++ Kadai3/ContentView.swift
@@ -8,10 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var number1: String = ""
+    @State var number2: String = ""
+    @State var isEquation1 = true
+    @State var isEquation2 = true
+
     var body: some View {
         VStack {
+                TextField("", text: $number1)
+                    .font(.title)
+                    .frame(width: 100)
+                    .keyboardType(.numberPad)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+            HStack{
+                Text("＋")
+                Toggle(isOn: $isEquation1) {
+                }
+                .fixedSize()
+                Text("ー")
+
+                // TextFieldで入力された値に＋もしくはーの符号をつける
+                if isEquation1 {
+                    Text("-" + String(number1))
+                        .font(.title)
+                }
+
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
