75d93d3d81103db076f852f93122a12aaecd4564
Ryuga <ribereo.minami@gmail.com>
2023-02-08 12:12:57 +0900

    トグルスイッチと入力された値の符号を変更

--- Kadai3/ContentView.swift
+++ Kadai3/ContentView.swift
@@ -15,26 +15,33 @@ struct ContentView: View {
 
     var body: some View {
         VStack {
-                TextField("", text: $number1)
-                    .font(.title)
-                    .frame(width: 100)
-                    .keyboardType(.numberPad)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
+            TextField("", text: $number1)
+                .font(.title)
+                .frame(width: 100)
+                .keyboardType(.numberPad)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            var num1 = Int(number1) ?? 0
+            //            var num2 = Int(number2) ?? 0
+
             HStack{
                 Text("＋")
                 Toggle(isOn: $isEquation1) {
                 }
                 .fixedSize()
                 Text("ー")
-
-                // TextFieldで入力された値に＋もしくはーの符号をつける
-                if isEquation1 {
-                    Text("-" + String(number1))
-                        .font(.title)
-                }
-
             }
-            .padding()
+            // TextFieldで入力された値に＋もしくはーの符号をつける
+            if isEquation1 {
+                // -() 入力された数字の符号を変える
+                var num1 = num1 * -1
+                Text(String(num1))
+                    .font(.title)
+            }
+            else {
+                // +() 入力された数字のまま
+                Text(String(num1))
+                    .font(.title)
+            }
         }
     }
 }
